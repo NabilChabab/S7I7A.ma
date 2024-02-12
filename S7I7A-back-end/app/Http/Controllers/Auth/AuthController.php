759ded3b279d61307f183d0ec68c8c707e0c240c
NabilChabab <nabil.chababnabil@gmail.com>
2024-02-12 08:43:30 +0100

    Auth

---
+++ S7I7A-back-end/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\Role;
+use App\Models\User;
+
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request) {
+        $validatedData = $request->validated();
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+    
+        $user->roles()->attach(Role::where('name', 'patient')->first()->id);
+    
+        Auth::login($user);
+    
+        return response()->json([
+            'message' => 'User registered successfully',
+            'redirect' => '/login',
+            'token' => $user->createToken('register')->plainTextToken
+        ], 201);
+    }
+
+    public function login(LoginRequest $request) {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+    
+        $user = Auth::user();
+    
+        if ($user->hasRole('Admin')) {
+            return response()->json([
+                'message' => 'Admin login successful',
+                'redirect' => '/admin/dashboard',
+                'role' => 'Admin',
+                'token' => $user->createToken('login')->plainTextToken
+            ], 200);
+        }
+    
+        elseif ($user->hasRole('Doctor')) {
+            return response()->json([
+                'message' => 'Doctor login successful',
+                'redirect' => '/doctor/dashboard',
+                'role' => 'Doctor',
+                'token' => $user->createToken('login')->plainTextToken
+            ], 200);
+        }
+
+        elseif ($user->hasRole('Patient')) {
+            return response()->json([
+                'message' => 'Patient login successful',
+                'redirect' => '/home',
+                'role' => 'Patient',
+                'token' => $user->createToken('login')->plainTextToken
+            ], 200);
+        }
+        else{
+            return response()->json([
+                'message' => 'User does not have a valid role',
+            ], 403);
+        }
+    
+    }
+    
+    
+
+    public function logout(Request $request){
+        Auth::user()->tokens->each(function ($token) {
+            $token->delete();
+        });
+    
+        return response()->json([
+            'message'=> 'Logout Successfully'
+        ]);
+    }
+}
+
---
+++ S7I7A-back-end/routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::middleware('role:admin')->get('/admin/dashboard', function () {
+        echo 'baby';
+    });
+
+    Route::middleware('role:doctor')->get('/doctor/dashboard', function () {
+        echo 'babydoctor';
+    });
+});
